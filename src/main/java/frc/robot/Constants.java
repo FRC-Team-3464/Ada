dc1200c7c92a13d7247d045b1f876ed9db28d311
Legion <Legion>
2023-05-30 20:24:59 -0400

    update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,30 +38,30 @@ public final class Constants {
   public static class DriveConstants{
 
     // Left and right wheel distance. 
-    public static final double kTrackWidth = Units.inchesToMeters(21);
+    public static final double kTrackWidth = Units.inchesToMeters(22);
     // Front and Back Wheel distance. 
-    public static final double kWheelBase = Units.inchesToMeters(25.5); // More long than wide. 
+    public static final double kWheelBase = Units.inchesToMeters(24); // More long than wide. 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-      new Translation2d(kWheelBase/2, -kTrackWidth/2), // Positition of mod 2 - front left
-      new Translation2d(kWheelBase/2, kTrackWidth/2), // Positition of mod 3 - front right
-      new Translation2d(-kWheelBase/2, -kTrackWidth/2), // Positition of mod 1 - back left
-      new Translation2d(-kWheelBase/2, kTrackWidth/2) // Positition of mod 0 - back right. 
+      new Translation2d(kWheelBase/2, -kTrackWidth/2), // Positition of mod 0 - front left
+      new Translation2d(kWheelBase/2, kTrackWidth/2), // Positition of mod 1 - front right
+      new Translation2d(-kWheelBase/2, -kTrackWidth/2), // Positition of mod 2 - back left
+      new Translation2d(-kWheelBase/2, kTrackWidth/2) // Positition of mod 3 - back right. 
     );
 
     // Actual Physical Robot Constraints:
     // Drive Meters 11.5 ft to meters: ~3.5 meters per second.
     public static final double kPhysicalMaxDriveSpeedMetersPerSecond = Units.feetToMeters(11.5); // According to https://www.andymark.com/products/swerve-and-steer
-    // Max speed: 1.5 Rotations per second * 2pi radians/one rotation
-    public static final double kPhysicalMaxTurningSpeedRadPerSecond = (90/60) * 2 * Math.PI; 
+    // Max speed: 1.5 Rotations per second (90 RPM) * 2pi radians/one rotation
+    public static final double kPhysicalMaxTurningSpeedRadPerSecond = 1.5 * 2 * Math.PI; 
 
     // Tele-Op Robot Constraints - change... it should be smaller than that. 
     // Dividing by four = splitting up the total speed into each of the modules: used to ensure that our swerve isn't too fast. 
     public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxDriveSpeedMetersPerSecond / 4;
     public static final double kTeleDriveMaxTurningSpeedRadPerSecond = kPhysicalMaxTurningSpeedRadPerSecond / 4;
 
-    // Watch out for this. 
-    public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 2.75;
-    public static final double kTeleTurningMaxAccelerationUnitsPerSecond = 2.5;
+    // Using comparative proportions, we get 3/5 = x/3.5
+    public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 2.1;
+    public static final double kTeleTurningMaxAccelerationUnitsPerSecond = 2.1;
     
 
 
